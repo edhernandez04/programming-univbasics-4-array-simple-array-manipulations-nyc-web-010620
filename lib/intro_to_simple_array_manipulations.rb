87358c65c1623409e14e52976bc8213cec8d25a6
Eddie Hernandez <mchh.004@gmail.com>
2019-12-27 23:00:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -56,6 +56,5 @@ def using_delete(array, string)
 end
 
 def using_delete_at(array, integer)
-  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-  deleted_robot = famous_robots.delete(-1)
+  deleted_robot = famous_robots.delete(2)
 end
