51a36fe634246e67627a97f1a7a82f6f01d10a7c
Eddie Hernandez <mchh.004@gmail.com>
2019-12-27 22:10:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,7 +31,7 @@ end
 def using_concat(array, things)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  my_favorite_things.concat!(more_favs)
+  my_favorite_things.concat(more_favs)
 end
 
 def using_insert(array, language)
