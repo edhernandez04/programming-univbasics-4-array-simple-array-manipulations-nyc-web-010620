84a1567abe86ce2a61b2dda9abc9832fbe93885c
Eddie Hernandez <mchh.004@gmail.com>
2019-12-27 21:56:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,8 +37,7 @@ end
 def using_insert
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   another_language = "Python"
-  list_of_programming_languages.insert(another_language, 4) = new_array
-  return new_array
+  list_of_programming_languages.insert(another_language, 4)
 end
 
 def using_uniq
