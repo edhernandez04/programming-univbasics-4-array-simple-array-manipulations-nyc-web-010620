0fb132aef1e2ce8818361d43211ab2d59fca5d14
Eddie Hernandez <mchh.004@gmail.com>
2019-12-27 22:04:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,61 +1,61 @@
-def using_push
+def using_push(array, color)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   colors_in_the_rainbow.push("violet")
 end
 
-def using_unshift
+def using_unshift(array, city)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   bouroughs_in_nyc.unshift("Staten Island")
 end
 
-def using_pop
+def using_pop(array)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   bouroughs_in_nyc.pop
 end
 
-def pop_with_args
+def pop_with_args(array)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  bouroughs_in_nyc.pop[2]
+  bouroughs_in_nyc.pop(2)
 end
 
-def using_shift
+def using_shift(array)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   bouroughs_in_nyc.shift
 end
 
-def shift_with_args
+def shift_with_args(array)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  bouroughs_in_nyc.shift[2]
+  bouroughs_in_nyc.shift(2)
 end
 
-def using_concat
+def using_concat(array, things)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  my_favorite_things.concat(more_favs)
+  my_favorite_things.concat!(more_favs)
 end
 
-def using_insert
+def using_insert(array, language)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   another_language = "Python"
   list_of_programming_languages.insert(another_language, 4)
 end
 
-def using_uniq
+def using_uniq(array)
   haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
   new_array = haircuts.uniq
 end
 
-def using_flatten
+def using_flatten(array)
   instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
   flat_array = instruments.flatten
 end
 
-def using_delete
+def using_delete(array, instructors)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   no_offense_steven = instructors.delete("Steven")
 end
 
-def using_delete_at
+def using_delete_at(array, instructors)
   famous_robots = ["Johnny 5", "R2D2", "Robocop"]
   deleted_robot = famous_robots.delete[2]
 end
